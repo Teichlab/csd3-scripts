e7efa086dccbe0be0b93e9715ddeb18d0bebddd5
ktpolanski <krzysztof_polanski@o2.pl>
2025-09-08 11:11:14 +0000

    vdj support

--- pyranger.py
+++ pyranger.py
@@ -11,8 +11,12 @@ def parse_args():
     parser.add_argument('--gex', dest='gex', type=str, default=None, help='Optional. GEX sample ID.')
     parser.add_argument('--cite', dest='cite', type=str, default=None, help='Optional. CITE sample ID.')
     parser.add_argument('--atac', dest='atac', type=str, default=None, help='Optional. ATAC sample ID.')
+    parser.add_argument('--tcrab', dest='atac', type=str, default=None, help='Optional. TCR-AB sample ID.')
+    parser.add_argument('--bcr', dest='atac', type=str, default=None, help='Optional. BCR sample ID.')
+    parser.add_argument('--tcrgd', dest='atac', type=str, default=None, help='Optional. ATAC sample ID.')
     parser.add_argument('--reference', dest='reference', type=str, required=True, help='Path to cellranger reference to use')
     parser.add_argument('--feature-ref', dest='feature_ref', type=str, default=None, help='CITE only. Path to feature reference file to use.')
+    parser.add_argument('--primers', dest='primers', type=str, default=None, help='VDJ only. Optional. Path to file with inner enrichment primers.')
     parser.add_argument('--chemistry', dest='chemistry', type=str, help='Optional. 10X chemistry argument to pass to Cellranger.')
     parser.add_argument('--cores', dest='cores', type=int, default=10, help='Number of cores to use. Default: 10')
     parser.add_argument('--no-bam', dest='no_bam', action='store_true', help='Flag. If provided, will skip creating BAMs in output if applicable.')
@@ -21,11 +25,11 @@ def parse_args():
     parser.add_argument('--no-link', dest='no_link', action='store_true', help='Flag. If provided, will skip creating symlinks in central sample storage for mappings.')
     args = parser.parse_args()
     #create a combined ID joining the various IDs present
-    samples = [args.gex, args.cite]
-    samples = [i for i in samples if i is not None]
-    if len(samples) == 0:
+    samples = [args.gex, args.cite, args.atac, args.tcrab, args.bcr, args.tcrgd]
+    args.samples = [i for i in samples if i is not None]
+    if len(args.samples) == 0:
         raise ValueError("At least one of the various sample IDs needs to be provided")
-    args.runid = "-".join(samples)
+    args.runid = "-".join(args.samples)
     if (args.cite is not None) and (args.feature_ref is None):
         raise ValueError("Need to specify feature reference for CITE")
     #set up path to csd3-scripts, i.e. where this is
@@ -98,6 +102,7 @@ def main():
             #we may have a feature reference to stick in there
             if args.feature_ref is not None:
                 cellranger_call.append("--feature-ref="+args.feature_ref)
+            #NOTE: pre-5.0.0 cellrangers don't have --no-bam support
             #cellranger 8.0.0 reworked --no-bam into a mandatory --create-bam= instead
             if compare_version(args.cellranger, "8.0.0"):
                 if args.no_bam:
@@ -107,6 +112,22 @@ def main():
             else:
                 if args.no_bam:
                     cellranger_call.append("--no-bam")
+    #vdj prep
+    if args.command == "vdj":
+        #easily pass fastqs as relative path
+        cellranger_call.append("--fastqs=fastq")
+        #pass reference
+        cellranger_call.append("--reference="+args.reference)
+        #set chain
+        if args.bcr is not None:
+            #we passed a BCR
+            cellranger_call.append("--chain=IG")
+        else:
+            #both TCR-AB and TCR-GD would qualify as TR per this
+            cellranger_call.append("--chain=TR")
+        #there may be inner enrichment primers
+        if args.primers is not None:
+            cellranger_call.append("--inner-enrichment-primers="+args.primers)
     #sort out chemistry
     if args.chemistry is not None:
         cellranger_call.append("--chemistry="+args.chemistry)
@@ -131,7 +152,7 @@ def main():
         script_lines.append("")
         script_lines.append("#at this point we're happy, I believe. let's symlink this over to the sample structure")
         script_lines.append("cd ..")
-        for present_sample in args.runid.split("-"):
+        for present_sample in args.samples:
             script_lines.append("bash "+args.location+"/makelinks.sh "+present_sample+" mapping")
     #script prep done! write it out!
     with open(args.script, "w") as fid:
