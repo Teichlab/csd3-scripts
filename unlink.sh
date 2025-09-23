7d3b8d745364118b15f50168855cca9592cdb0bd
ktpolanski <krzysztof_polanski@o2.pl>
2025-09-23 12:42:26 +0000

    symlink remover

---
+++ unlink.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eo pipefail
+
+#helper function for removing a symlink to . from the sample structure
+
+#two positional arguments - the sample ID and the name of the subfolder
+SAMPLE=$1
+SUBFOLDER=$2
+
+#we need the hashes
+PROJECT=`echo ${SAMPLE} | cut -f 1 -d "_"`
+SHASH=`echo ${SAMPLE} | cut -f 2 -d "_" | cut -c 1-5`
+#-L is the required syntax to see if there's a thing and it's a symlink
+FOLDERNAME=$(basename `realpath .`)
+if [ -L /rds/project/rds-C9woKbOCf2Y/samples/${PROJECT}/${SHASH}/${SAMPLE}/${SUBFOLDER}/${FOLDERNAME} ]
+then
+    #get rid of the symlink
+    rm /rds/project/rds-C9woKbOCf2Y/samples/${PROJECT}/${SHASH}/${SAMPLE}/${SUBFOLDER}/${FOLDERNAME}
+fi
