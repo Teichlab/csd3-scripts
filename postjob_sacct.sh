7d5303a9b7fd96f79bec7a55eb2c0d81e911bb4f
ktpolanski <krzysztof_polanski@o2.pl>
2026-02-24 10:32:34 +0000

    move to seff

--- postjob_sacct.sh
+++ postjob_sacct.sh
@@ -7,5 +7,8 @@ JOBID=$1
 #retrieve path to the job's log file via scontrol
 LOGFILE=$(scontrol show job ${JOBID} | grep StdOut | cut -f 2 -d "=")
 
-#park sacct at the end of the log file
-sacct -j "${JOBID}" --format=JobID,State,ExitCode,AllocCPUS,ReqCPUS,ReqMem,Timelimit,Elapsed,MaxRSS >> ${LOGFILE}
+#park seff at the end of the log file as it's more useful than sacct
+seff ${JOBID} >> ${LOGFILE}
+
+#here's the archival sacct syntax just in case
+#sacct -j "${JOBID}" --format=JobID,State,ExitCode,AllocCPUS,ReqCPUS,ReqMem,Timelimit,Elapsed,MaxRSS >> ${LOGFILE}
